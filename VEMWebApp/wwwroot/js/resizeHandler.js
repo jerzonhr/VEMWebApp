25e58ed742a106cb8d5f6369d3f70686e889f35b
Jerzon Hernandez <jerzon.hz@gmail.com>
2025-03-17 09:45:08 -0600

    Add Navigation Drawer auto hide on window resize

---
+++ VEMWebApp/wwwroot/js/resizeHandler.js
@@ -0,0 +1,8 @@
+﻿export function registerResizeHandler(dotNetHelper) {
+    function resizeListener() {
+        dotNetHelper.invokeMethodAsync('OnResize', window.innerWidth);
+    }
+    window.addEventListener('resize', resizeListener);
+    // Call it once to set the initial state
+    resizeListener();
+}
